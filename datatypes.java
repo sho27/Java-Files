d5c7f6b1a597c0685a3beaae50376efb9ce90736
sho27 <87562342+sho27@users.noreply.github.com>
2022-01-30 21:21:31 +0545

    Update class name

--- datatypes.java
+++ datatypes.java
@@ -1,5 +1,5 @@
 import java.util.*;
-public class datatypes{
+public class DataTypes{
     public static void main(String args[]) {
         Scanner scan = new Scanner(System.in);
         System.out.println("Enter an Integer, Double and String");
