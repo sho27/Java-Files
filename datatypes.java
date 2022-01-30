1269cb2970af691843d90507332904b998d82dee
sho27 <87562342+sho27@users.noreply.github.com>
2022-01-30 17:35:16 +0545

    Data types

---
+++ datatypes.java
@@ -0,0 +1,14 @@
+import java.util.*;
+public class datatypes{
+    public static void main(String args[]) {
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Enter an Integer, Double and String");
+        int i = scan.nextInt();
+        double d = scan.nextDouble();
+        String s = scan.nextLine();
+        System.out.println("Integer :" + i);
+        System.out.println("Double :" + d);
+        System.out.println("String :" + s);
+        scan.close();
+    }
+}
