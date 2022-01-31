eed7a4bcb8171d136e9135f47e838c2fa6dc0da6
sho27 <87562342+sho27@users.noreply.github.com>
2022-01-31 16:27:41 +0545

    changes according to Naming Conventions

--- datatypes.java
+++ datatypes.java
@@ -3,12 +3,12 @@ public class DataTypes{
     public static void main(String args[]) {
         Scanner scan = new Scanner(System.in);
         System.out.println("Enter an Integer, Double and String");
-        int i = scan.nextInt();
-        double d = scan.nextDouble();
-        String s = scan.nextLine();
-        System.out.println("Integer :" + i);
-        System.out.println("Double :" + d);
-        System.out.println("String :" + s);
+        int i_integer = scan.nextInt();
+        double d_double = scan.nextDouble();
+        String s_string = scan.nextLine();
+        System.out.println("Integer :" + i_integer);
+        System.out.println("Double :" + d_double);
+        System.out.println("String :" + s_string);
         scan.close();
     }
 }
