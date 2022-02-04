28f7a01866c8390b040ed39fb4df1bc332adb3f7
sho27 <87562342+sho27@users.noreply.github.com>
2022-02-04 10:52:14 +0545

    Switch Statement

---
+++ SwitchStatement.java
@@ -0,0 +1,23 @@
+import java.util.*;
+public class SwitchStatement {
+     public static void main(String args[]) {
+        Scanner scan = new Scanner(System.in); 
+        int n = scan.nextInt();
+         switch(n){
+            case 1: System.out.println("One");
+                    break;
+            case 2: System.out.println("Two");
+                    break;
+            case 3: System.out.println("Three");
+                    break;
+            case 4: System.out.println("Four");
+                    break;
+            case 5: System.out.println("Five");
+                    break;
+            default:
+                    System.out.println("No match");
+                    break;
+         }
+         scan.close();
+     }
+}
