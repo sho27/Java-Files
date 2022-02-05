8ec3d020babb9591e4df4cd84f8ac31a3736ed13
sho27 <87562342+sho27@users.noreply.github.com>
2022-02-05 17:31:18 +0545

    sum of even numbers between 1 to 100

---
+++ SumEven.java
@@ -0,0 +1,11 @@
+public class SumEven {
+    public static void main(String args[]) {
+        int sum = 0;
+        for (int i = 1; i <= 100; i++) {
+            if (i%2 == 0) {
+              sum += i;  
+            }
+        }
+        System.out.println("Sum of Even numbers between 1 to 100 : " + sum);
+    }  
+}
