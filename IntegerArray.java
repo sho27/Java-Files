0aca1fb06f28752e3fdf6bbca871c0c176c7f5d6
sho27 <87562342+sho27@users.noreply.github.com>
2022-02-09 22:57:34 +0545

    implemented declaration, assigning and accessing Integer Arrays

---
+++ IntegerArray.java
@@ -0,0 +1,18 @@
+public class IntegerArray {
+    public static void main(String args[]) {
+        //Declare an array
+        int arr[] = new int[5];
+        
+        //assign elements to array
+        arr[0] = 3;
+        arr[1] = 7;
+        arr[2] = 1;
+        arr[3] = 9;
+        arr[4] = 8;
+
+        //Accessing the elements of the array
+        for (int i = 0; i < 5; i++) {
+            System.out.println(arr[i]);
+        }
+    }
+}
