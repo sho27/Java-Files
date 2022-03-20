f2feb2ce605cd86359857a087b811fb8b3001ae1
sho27 <87562342+sho27@users.noreply.github.com>
2022-03-21 00:38:09 +0545

    implemented a program to display a record of a database table (based on specific field).

---
+++ DisplayRecord.java
@@ -0,0 +1,26 @@
+import java.sql.*;
+public class DisplayRecord 
+{
+	public static void main(String[] args) throws Exception
+	{
+		String url = "jdbc:mysql://localhost:3306/school";
+		String uname = "root";
+		String pass = "root";
+		String query ="select * from student where rollno=3";
+		
+		Class.forName("com.mysql.cj.jdbc.Driver");
+		Connection con = DriverManager.getConnection(url, uname, pass);
+		Statement st = con.createStatement();
+		ResultSet rs = st.executeQuery(query);
+		
+		while(rs.next())
+		{
+			int id = rs.getInt("rollno");
+			String name = rs.getString("sname");		
+			System.out.println(id + " " + name);
+		}
+		
+		st.close();
+		con.close();
+	}
+}
